b21d3289b1183bf1b2eea5107f293aec32d321a7
Andry Eroshkin <drontosaur@ya.ru>
2012-09-23 22:19:58 +0400

    "First Fail" Achieved

--- main.c
+++ main.c
@@ -3,12 +3,21 @@
 #include <winsock2.h>
 #include <ws2tcpip.h>
 #include <iphlpapi.h>
+#include <windows.h>
+#include "libws2_32.a"
 
-#pragma comment(lib, "Ws2_32.lib") //говорим компилятору что нужна библиотека Ws2_32.lib
+#pragma comment(lib, "libws2_32.a") //говорим компилятору что нужна библиотека Ws2_32.lib
 
 int
 main(void)
 {
+    WSADATA wsaData; //инициализмруем библиотеку Ws2_32.lib
+    int res = WSAStartup(MAKEWORD(2,2), &wsaData);
+    if (res)
+    {
+        printf("Unable to initiate lib, tard\n");
+        return 0;
+    }
 
     return 0;
 }
