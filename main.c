b837e32548d409b33b2e4cb64729c86f2d81c52f
Andry Eroshkin <drontosaur@ya.ru>
2012-09-23 21:20:12 +0400

    Right headers and libraries are included

--- main.c
+++ main.c
@@ -1,9 +1,14 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <winsock2.h>
+#include <ws2tcpip.h>
+#include <iphlpapi.h>
+
+#pragma comment(lib, "Ws2_32.lib") //говорим компилятору что нужна библиотека Ws2_32.lib
 
 int
 main(void)
 {
-    printf("Hello world!\n");
+
     return 0;
 }
