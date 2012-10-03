fc042b657ae39827457e7afef04fd41340268dc5
Andry Eroshkin <drontosaur@ya.ru>
2012-10-04 02:23:20 +0400

    It's ALIVE!!11!!!11!!!!!

--- The_Chat.pro
+++ The_Chat.pro
@@ -1,6 +1,6 @@
 TEMPLATE = app
 CONFIG += console
 CONFIG -= qt
-
+LIBS += C:\Users\User\Desktop\Dront\Prog\The-Chat\WS2_32.Lib
 SOURCES += main.c
 
--- main.c
+++ main.c
@@ -1,8 +1,23 @@
+#include <windows.h>
+#include <winsock2.h>
+#include <ws2tcpip.h>
+#include <stdlib.h>
 #include <stdio.h>
 
 int main(void)
 {
-    printf("Hello World!\n");
+    WSADATA wsaData;
+    int iResult;
+
+    // Initialize Winsock
+    iResult = WSAStartup(MAKEWORD(2,2), &wsaData);
+    if (iResult != 0) {
+        printf("WSAStartup failed: %d\n", iResult);
+        return 1;
+    }
+
+
+    printf("You Suck!\n");
     return 0;
 }
 
